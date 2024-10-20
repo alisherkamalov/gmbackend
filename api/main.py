1ee9f408375b33bc8357509b91341b0960d26a49
Alisher <161970644+alisherkamalov@users.noreply.github.com>
2024-10-20 21:40:39 +0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -12,38 +12,23 @@ def download_audio():
     if not video_url:
         return jsonify({'error': 'URL is required'}), 400
 
-    # Путь для сохранения аудио
-    audio_path = f"downloads/{os.path.basename(video_url)}.mp3"
-
     # Настройки для yt-dlp
     ydl_opts = {
         'format': 'bestaudio/best',
         'extractaudio': True,
         'audioformat': 'mp3',
-        'outtmpl': audio_path,
         'noplaylist': True,
         'quiet': True,
     }
 
     try:
-        # Загружаем аудио
+        # Загружаем аудио и получаем файл
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([video_url])
+            info_dict = ydl.extract_info(video_url, download=True)
+            audio_url = info_dict['formats'][0]['url']  # Получаем ссылку на загруженное аудио
 
-        # Проверяем, что файл был создан
-        if os.path.exists(audio_path):
-            # Возвращаем ссылку на файл
-            response = jsonify({'audio_url': audio_path})
-            # Удаляем файл после отправки ответа
-            @response.call_on_close
-            def remove_file():
-                try:
-                    os.remove(audio_path)
-                    print(f'Файл {audio_path} успешно удален.')
-                except Exception as e:
-                    print(f'Ошибка при удалении файла: {str(e)}')
-
-            return response, 200
+        if audio_url:
+            return jsonify({'audio_url': audio_url}), 200
         else:
             return jsonify({'error': 'Ошибка загрузки аудиофайла'}), 500
 
@@ -51,6 +36,4 @@ def download_audio():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    # Создаем директорию для загрузок, если ее нет
-    os.makedirs('downloads', exist_ok=True)
     app.run(debug=True, port=5000)
